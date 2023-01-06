db21abdef46c7805f81302d26075eabd80501e8c
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-06 21:37:50 +0530

    Sum_of_integers

---
+++ Introduction/Sum_of_integers.c
@@ -0,0 +1,10 @@
+ // program to take multiple inputs in a single line    
+ #include<stdio.h>
+ int main(){
+    int a,b,c,d;
+    printf("enter 4 numbers:");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    int sum=a+b+c+d;
+    printf("\n answer= %d",sum);
+    return 0;
+ }                                                                                                                                                
