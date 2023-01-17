b2b770413f640e042b4411bf26318a0a1fd50fde
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-17 21:40:04 +0530

    Indroduction

--- Introduction/Introduction.c
+++ Introduction/Introduction.c
@@ -1,5 +1,7 @@
 #include <stdio.h>    
 int main(){    
-    printf("Hello C Language");    
-    return 0;   
+    // printf("Hello C Language");    
+    // return 0;   
+    int var = 010;
+    printf("%d",var);
 }  
