3e366b23baff371509c6ef0d422a3a97f9873c34
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-20 16:06:10 +0530

    Even number printing

---
+++ Loops/Even_number_printing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enten any number: ");
+    scanf("%d",&n);
+    printf("The even number from 1 to %d : \n",n);
+    for(int i = 1; i<=n; i++){
+        if(i%2 == 0){
+            printf("%d ",i);
+        }
+    }
+}
